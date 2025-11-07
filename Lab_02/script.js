fa3fff1b27a50e2af171774caf205129eb55af1d
Szymon <spytel0128@gmail.com>
2025-11-07 22:21:04 +0100

    First project

---
+++ Lab_02/script.js
@@ -0,0 +1,9 @@
+/* === SCROLLING === */
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
